5a048a4a2370c5fc3379631e35a5cdd1921aca70
Taco <79993302+Just-Taco@users.noreply.github.com>
2023-07-15 00:04:17 +0200

    Delete server.mjs

--- server.mjs
+++
@@ -1,8 +0,0 @@
-// alt:V built-in module that provides natives API (functions from GTA V).
-import * as alt from 'alt-server';
-
-import * as chat from 'alt:chat';
-
-chat.registerCmd('notify', (player, args) => {
-  alt.emitClient(player, 'server:notify', 'you just won the lottery', 2000, 'success');
-}, false);
